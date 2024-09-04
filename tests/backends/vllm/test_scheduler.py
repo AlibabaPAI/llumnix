19693d2f817611361e6d9b2bb3d29c5c83e0381b
xianyu <cuikuilong.ckl@alibaba-inc.com>
2024-09-04 08:11:09 +0000

    fix

--- tests/backends/vllm/test_scheduler.py
+++ tests/backends/vllm/test_scheduler.py
@@ -11,8 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
-
 from vllm.sequence import Sequence
 from vllm.sequence import Logprob
 from llumnix.backends.vllm.scheduler import BlockManagerLlumnix

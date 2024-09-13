c8f9e09f00d4399ff9fd25925ba698a52790511d
s5u13b <sunbiao.sun@alibaba-inc.com>
2024-09-13 09:47:08 +0000

    Change request migration policy FWSR to FWJ

--- llumnix/arg_utils.py
+++ llumnix/arg_utils.py
@@ -162,7 +162,7 @@ class EngineManagerArgs:
         parser.add_argument('--request-migration-policy',
                             type=str,
                             default=None,
-                            choices=['LCFS', 'SRF', 'LRF', 'FWSR'],
+                            choices=['LCFS', 'SRF', 'LRF', 'FWJ'],
                             help='request migration policy')
         parser.add_argument('--enable-defrag',
                             type=bool,

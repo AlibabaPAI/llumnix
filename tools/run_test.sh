d80c47898ac784eb7a26e919ced23483a60f037c
s5u13b <sunbiao.sun@alibaba-inc.com>
2024-11-22 09:21:09 +0000

    Fix kill in ci

--- tools/run_test.sh
+++ tools/run_test.sh
@@ -3,8 +3,8 @@ test_mode=$1
 
 set -ex
 
-pkill -f llumnix.entrypoints.vllm.api_server
-pkill -f benchmark_serving.py
+pgrep -f llumnix.entrypoints.vllm.api_server | { while read pid; do kill -9 "$pid"; done; }
+pgrep -f benchmark_serving.py | { while read pid; do kill -9 "$pid"; done; }
 
 nvidia-docker run --rm -t --net host --ipc host -v ${PWD}:/workspace -v /mnt:/mnt -w /workspace \
   registry.cn-beijing.aliyuncs.com/llumnix/llumnix-dev:20240909_action_678a439 \

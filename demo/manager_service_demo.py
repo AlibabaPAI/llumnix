c94b5448ea01be689717995d6b7ac73ae9662537
s5u13b <sunbiao.sun@alibaba-inc.com>
2024-12-23 09:45:16 +0000

    Refine codes

--- demo/manager_service_demo.py
+++ demo/manager_service_demo.py
@@ -1,6 +1,6 @@
 import asyncio
-from queue import Queue
 import time
+import traceback
 from typing import Dict
 from functools import partial
 import uvicorn
@@ -11,6 +11,9 @@ from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 from ray.util.queue import Queue as RayQueue
 
 from llumnix.utils import random_uuid
+from llumnix.logger import init_logger
+
+logger = init_logger(__name__)
 
 PLACEMENT_GROUP_NAME_PREFIX = "pg_"
 SERVER_NAME_PREFIX = "server_"
@@ -28,12 +31,54 @@ def get_server_name(instance_id: str) -> str:
 def get_instance_name(instance_id: str) -> str:
     return f"{INSTANCE_NAME_PREFIX}{instance_id}"
 
-def initialize_placement_group(lifetime: str = None, instance_id: str = None) -> PlacementGroup:
+def initialize_placement_group(instance_id: str = None, lifetime: str = None) -> PlacementGroup:
     placement_group_specs = ([{"CPU": 1}, {"CPU": 1, "GPU": 4}])
     placement_group_name = get_placement_group_name(instance_id)
     placement_group = ray.util.placement_group(
         placement_group_specs, "STRICT_PACK", lifetime=lifetime, name=placement_group_name)
     return placement_group
+
+def remove_placement_group(instance_id: str = None) -> bool:
+    placement_group = ray.util.get_placement_group(get_placement_group_name(instance_id))
+    if not placement_group:
+        return False
+    try:
+        ray.util.remove_placement_group(placement_group)
+    # pylint: disable=broad-except
+    except Exception:
+        return False
+    return True
+
+def kill_server(instance_id: str = None) -> bool:
+    try:
+        server = ray.get_actor(get_server_name(instance_id))
+    except ValueError:
+        return False
+    try:
+        ray.kill(server)
+    # pylint: disable=broad-except
+    except Exception:
+        return False
+    return True
+
+def kill_instance(instance_id: str = None) -> bool:
+    try:
+        instance = ray.get_actor(get_instance_name(instance_id))
+    except ValueError:
+        return False
+    try:
+        ray.kill(instance)
+        return True
+    # pylint: disable=broad-except
+    except Exception:
+        return False
+
+def actor_exists(actor_name: str) -> bool:
+    try:
+        ray.get_actor(actor_name)
+        return True
+    except ValueError:
+        return False
 
 
 class FastAPIServer:
@@ -106,7 +151,7 @@ class LLMEngineManager:
         self.instances: Dict[str, Llumlet] = {}
         asyncio.create_task(self._auto_scale_up_loop())
         asyncio.create_task(self._auto_scale_down_loop())
-        asyncio.create_task(self._check_deployment_loop())
+        asyncio.create_task(self._check_deployment_correctness_loop())
         print("LLMEngineManager created")
 
     async def _auto_scale_down_loop(self) -> None:
@@ -117,105 +162,78 @@ class LLMEngineManager:
                 self._scale_down(instance_id)
 
         while True:
-            tasks = []
-            for instance_id, instance in self.instances.items():
-                task = asyncio.gather(instance.ready.remote(), return_exceptions=True)
-                task.add_done_callback(partial(instance_ready_callback, instance_id))
-                tasks.append(task)
-            await asyncio.gather(*tasks, return_exceptions=True)
+            try:
+                tasks = []
+                for instance_id, instance in self.instances.items():
+                    task = asyncio.gather(instance.ready.remote(), return_exceptions=True)
+                    task.add_done_callback(partial(instance_ready_callback, instance_id))
+                    tasks.append(task)
+                await asyncio.gather(*tasks, return_exceptions=True)
 
-            await asyncio.sleep(AUTO_DEPLOYMENT_INTERVAL_SECONDS)
-
-    async def _auto_scale_up_loop_removed(self) -> None:
-        def instance_ready_callback(instance_id: str, fut):
-            ret = fut.result()[0]
-            if not isinstance(ret, ray.exceptions.RayActorError):
-                print(f"instance {instance_id} ready, scale up")
-                self._scale_up(instance_id, new_pgs[instance_id], new_servers[instance_id], new_instances[instance_id])
-                new_servers[instance_id].run.remote()
-            else:
-                print(f"instance {instance_id} died, abort")
-                self._remove_placement_group(new_pgs[instance_id], instance_id)
-                self._kill_server(new_servers[instance_id], instance_id)
-
-        while True:
-            # 1. Get new placement group continuously until wait placement group ready timeout.
-            new_pg_queue = Queue()
-            while True:
-                new_pg = initialize_placement_group(lifetime="detached") if not self.last_pending_pg else self.last_pending_pg
-                try:
-                    await asyncio.wait_for(new_pg.ready(), timeout=WAIT_PLACEMENT_GROUP_TIMEOUT_SECONDS)
-                    new_pg_queue.put(new_pg)
-                except asyncio.TimeoutError:
-                    print("Wait new placement group ready timeout")
-                    self.last_pending_pg = new_pg
-                    break
-            print("Get new placement group ready done")
-
-            # 2. Deploy 1 server and 1 instance to 1 placement group (for all new placement groups).
-            new_pgs: Dict[str, PlacementGroup] = {}
-            new_servers: Dict[str, FastAPIServer] = {}
-            new_instances: Dict[str, Llumlet] = {}
-            while not new_pg_queue.empty():
-                instance_id = random_uuid()
-                new_pg = new_pg_queue.get()
-                new_pgs[instance_id] = new_pg
-                new_servers[instance_id] = FastAPIServer.from_args(instance_id, self.host, self.port, new_pg, lifetime="detached")
-                new_instances[instance_id] = Llumlet.from_args(instance_id, new_pg, lifetime="detached")
-            print("Deploy server and instance done")
-
-            # 3. Wait for all instances ready. (With the assumption that once instance ready, server is ready too.)
-            tasks = []
-            for instance_id, instance in new_instances.items():
-                task = asyncio.gather(instance.ready.remote(), return_exceptions=True)
-                task.add_done_callback(partial(instance_ready_callback, instance_id))
-                tasks.append(task)
-            await asyncio.gather(*tasks, return_exceptions=True)
-            print("Wait all instances ready done")
-
-            await asyncio.sleep(AUTO_DEPLOYMENT_INTERVAL_SECONDS)
+                await asyncio.sleep(AUTO_DEPLOYMENT_INTERVAL_SECONDS)
+            # pylint: disable=broad-except
+            except Exception as e:
+                logger.error("unexpected exception occurs: {}".format(e))
+                logger.error("exception traceback: {}".format(traceback.format_exc()))
 
     async def _auto_scale_up_loop(self) -> None:
         while True:
-            pending_pg_states = ray.util.state.list_placement_groups(filters=[("state", "=", "PENDING")])
-            while len(pending_pg_states) > 1:
-                self._remove_placement_group(ray.util.get_placement_group(pending_pg_states.pop()["name"]))
-            new_pg = initialize_placement_group(lifetime="detached") if len(pending_pg_states) == 0 else pending_pg_states[0]
             try:
-                await asyncio.wait_for(new_pg.ready(), timeout=WAIT_PLACEMENT_GROUP_TIMEOUT_SECONDS)
-                print("Get new placement group ready done")
-                instance_id = random_uuid()
-                self._initialize_server_and_instance(instance_id, new_pg)
-                print("Deploy server and instance to placement group done")
-            except asyncio.TimeoutError:
-                print("Wait new placement group ready timeout")
-                await asyncio.sleep(AUTO_DEPLOYMENT_INTERVAL_SECONDS)
-
-    async def _check_deployment_loop(self) -> None:
-        while True:
-            curr_pgs: Dict[str, PlacementGroup] = {}
-            curr_servers: Dict[str, PlacementGroup] = {}
-            curr_instances: Dict[str, Llumlet] = {}
-            created_pg_states = ray.util.state.list_placement_groups(filters=[("state", "=", "CREATED")])
-            for created_pg_state in created_pg_states:
-                instance_id = created_pg_state["name"].split("_")[-1]
-                curr_pgs[instance_id] = ray.util.get_placement_group(created_pg_state["name"])
-            alive_actor_states = ray.util.state.list_actors(filters=[("state", "=", "ALIVE")])
-            for alive_actor_state in alive_actor_states:
-                if alive_actor_state["name"].startswith(SERVER_NAME_PREFIX):
-                    instance_id = alive_actor_state["name"].split("_")[-1]
-                    curr_servers[instance_id] = ray.get_actor(alive_actor_state["name"])
-                elif alive_actor_state["name"].startswith(INSTANCE_NAME_PREFIX):
-                    instance_id = alive_actor_state["name"].split("_")[-1]
-                    curr_instances[instance_id] = ray.get_actor(alive_actor_state["name"])
-
-            assert len(curr_pgs) > max(len(curr_servers), len(curr_instances))
-
-            for instance_id in curr_pgs:
-                if instance_id not in curr_servers or instance_id not in curr_instances:
+                pending_pg_states = ray.util.state.list_placement_groups(filters=[("state", "=", "PENDING")])
+                for pending_pg_state in pending_pg_states:
+                    instance_id = pending_pg_state["name"].split("_")[-1]
                     self._scale_down(instance_id)
+                instance_id = random_uuid()
+                new_pg = initialize_placement_group(instance_id, lifetime="detached")
+                await new_pg.ready()
+                print("Get new placement group ready done")
+                self._initialize_server_and_instance(instance_id, new_pg)
+                print("Deploy server and instance to new placement group done")
+            # pylint: disable=broad-except
+            except Exception as e:
+                logger.error("unexpected exception occurs: {}".format(e))
+                logger.error("exception traceback: {}".format(traceback.format_exc()))
 
-            await asyncio.sleep(AUTO_DEPLOYMENT_INTERVAL_SECONDS)
+    async def _check_deployment_correctness_loop(self) -> None:
+        while True:
+            try:
+                curr_pgs: Dict[str, PlacementGroup] = {}
+                curr_servers: Dict[str, PlacementGroup] = {}
+                curr_instances: Dict[str, Llumlet] = {}
+                created_pg_states = ray.util.state.list_placement_groups(filters=[("state", "=", "CREATED")])
+                for created_pg_state in created_pg_states:
+                    instance_id = created_pg_state["name"].split("_")[-1]
+                    curr_pgs[instance_id] = ray.util.get_placement_group(created_pg_state["name"])
+                alive_actor_states = ray.util.state.list_actors(filters=[("state", "=", "ALIVE")])
+                for alive_actor_state in alive_actor_states:
+                    if alive_actor_state["name"].startswith(SERVER_NAME_PREFIX):
+                        instance_id = alive_actor_state["name"].split("_")[-1]
+                        curr_servers[instance_id] = ray.get_actor(alive_actor_state["name"])
+                    elif alive_actor_state["name"].startswith(INSTANCE_NAME_PREFIX):
+                        instance_id = alive_actor_state["name"].split("_")[-1]
+                        curr_instances[instance_id] = ray.get_actor(alive_actor_state["name"])
+
+                assert len(curr_pgs) > max(len(curr_servers), len(curr_instances))
+
+                for instance_id in curr_pgs:
+                    if instance_id not in curr_servers or instance_id not in curr_instances:
+                        self._scale_down(instance_id)
+                        if instance_id in curr_pgs:
+                            curr_pgs.pop(instance_id)
+                        if instance_id in curr_servers:
+                            curr_servers.pop(instance_id)
+                        if instance_id in curr_instances:
+                            curr_instances.pop(instance_id)
+
+                self.pgs = curr_pgs
+                self.servers = curr_servers
+                self.instance = curr_instances
+
+                await asyncio.sleep(AUTO_DEPLOYMENT_INTERVAL_SECONDS)
+            # pylint: disable=broad-except
+            except Exception as e:
+                logger.error("unexpected exception occurs: {}".format(e))
+                logger.error("exception traceback: {}".format(traceback.format_exc()))
 
     def _initialize_server_and_instance(self, instance_id: str, placement_group: PlacementGroup):
         def instance_ready_callback(instance_id: str, fut):
@@ -226,8 +244,8 @@ class LLMEngineManager:
                 new_server.run.remote()
             else:
                 print(f"instance {instance_id} died, abort scale up")
-                self._remove_placement_group(placement_group, instance_id)
-                self._kill_server(new_server, instance_id)
+                remove_placement_group(instance_id)
+                kill_server(instance_id)
 
         new_server = FastAPIServer.from_args(instance_id, self.host, self.port, placement_group, lifetime="detached")
         new_instance = Llumlet.from_args(instance_id, placement_group, lifetime="detached")
@@ -248,21 +266,9 @@ class LLMEngineManager:
         self.instances[instance_id] = instance
 
     def _scale_down(self, instance_id: str) -> None:
-        try:
-            server = ray.get_actor(get_server_name(instance_id))
-            self._kill_server(server, instance_id)
-        except ValueError:
-            pass
-        try:
-            instance = ray.get_actor(get_instance_name(instance_id))
-            self._kill_instance(instance, instance_id)
-        except ValueError:
-            pass
-        try:
-            placement_group = ray.util.get_placement_group(get_placement_group_name(instance_id))
-            self._remove_placement_group(placement_group, instance_id)
-        except ValueError:
-            pass
+        kill_server(instance_id)
+        kill_instance(instance_id)
+        remove_placement_group(instance_id)
         if instance_id in self.pgs:
             print(f"del placement group {instance_id}")
             del self.pgs[instance_id]
@@ -272,35 +278,6 @@ class LLMEngineManager:
         if instance_id in self.instances:
             print(f"del instance {instance_id}")
             del self.instances[instance_id]
-
-    def _remove_placement_group(self,
-                                placement_group: PlacementGroup,
-                                instance_id: str = None) -> None:
-        try:
-            ray.util.remove_placement_group(placement_group)
-        # pylint: disable=broad-except
-        except Exception:
-            print(f"try to remove placement group {instance_id}")
-
-    def _kill_server(self,
-                     server: FastAPIServer,
-                     instance_id: str = None) -> None:
-        try:
-            ray.kill(server)
-        # Exception that killing a died actor.
-        # pylint: disable=broad-except
-        except Exception:
-            print(f"try to kill api server {instance_id}")
-
-    def _kill_instance(self,
-                       instance: Llumlet,
-                       instance_id: str = None) -> None:
-        try:
-            ray.kill(instance)
-        # Exception that killing a died actor.
-        # pylint: disable=broad-except
-        except Exception:
-            print(f"try to kill instance {instance_id}")
 
     @classmethod
     def from_args(cls):

907050c2f5ada3adb32cab752406cb0fdd3a3db1
s5u13b <sunbiao.sun@alibaba-inc.com>
2024-12-24 06:52:49 +0000

    Simplify deployment

--- examlpes/offline_inference.py
+++ examlpes/offline_inference.py
@@ -51,6 +51,13 @@ instance_ids, llumlets = ray.get(manager.init_llumlets.remote(
 
 ray.get(manager.scale_up.remote(instance_ids, llumlets))
 
+# Create llumlets.
+llumlet_ids: List[str] = None
+llumlets: List[Llumlet] = None
+llumlet_ids, llumlets = ray.get(engine_manager.init_llumlets.remote(
+    engine_args, QueueType("rayqueue"), BackendType.VLLM, 1,
+))
+
 # The requests‘ outputs will be put to the request_output_queue no matter which instance it's running in.
 server_id = random_uuid()
 request_output_queue = RayQueueServer()

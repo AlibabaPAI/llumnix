96b8490a517b0f2ea5c417afc7b40ce71a0f0821
s5u13b <sunbiao.sun@alibaba-inc.com>
2025-03-05 10:28:07 +0000

    Add many one-to-one unit test

--- tests/unit_test/backends/vllm/test_migration_backend.py
+++ tests/unit_test/backends/vllm/test_migration_backend.py
@@ -26,10 +26,10 @@ from llumnix.utils import random_uuid, initialize_placement_group, get_placement
 from tests.conftest import ray_env
 from .test_worker import create_worker
 
-def get_ready_workers(num_worker, num_gpu_blocks, engine_config, migraiton_config):
+def get_ready_workers(num_workers, num_gpu_blocks, engine_config, migraiton_config):
     workers = []
     worker_ids = []
-    for _ in range(num_worker):
+    for _ in range(num_workers):
         worker_id = random_uuid()
         worker = create_worker(rank=0, local_rank=0, engine_config=engine_config,
                                worker_module_name="tests.unit_test.backends.vllm.test_migration_backend",
@@ -84,9 +84,9 @@ def test_one_to_many_migrate_cache(ray_env, backend, migration_num_buffers):
                                     migration_num_buffers=3).create_migration_config()
     migraiton_config.migration_backend = backend
 
-    num_worker = 4
+    num_workers = 4
     num_gpu_blocks = 6000
-    workers, _ = get_ready_workers(num_worker, num_gpu_blocks, engine_config, migraiton_config)
+    workers, _ = get_ready_workers(num_workers, num_gpu_blocks, engine_config, migraiton_config)
 
     num_layers = engine_config.model_config.get_num_layers(engine_config.parallel_config)
     head_size = engine_config.model_config.get_head_size()
@@ -100,7 +100,7 @@ def test_one_to_many_migrate_cache(ray_env, backend, migration_num_buffers):
     dst_blocks = list(range(num_gpu_blocks))
     random.shuffle(dst_blocks)
 
-    single_worker_num_blocks = len(dst_blocks)//(num_worker-1)
+    single_worker_num_blocks = len(dst_blocks)//(num_workers-1)
     migration_tasks = []
     worker_idx = 1
     per_step_blocks = 500
@@ -138,9 +138,9 @@ def test_many_to_one_migrate_cache(ray_env, backend):
                                     migration_num_buffers=3).create_migration_config()
     migraiton_config.migration_backend = backend
 
-    num_worker = 4
+    num_workers = 4
     num_gpu_blocks = 6000
-    workers, _ = get_ready_workers(num_worker, num_gpu_blocks, engine_config, migraiton_config)
+    workers, _ = get_ready_workers(num_workers, num_gpu_blocks, engine_config, migraiton_config)
 
     num_layers = engine_config.model_config.get_num_layers(engine_config.parallel_config)
     head_size = engine_config.model_config.get_head_size()
@@ -149,14 +149,14 @@ def test_many_to_one_migrate_cache(ray_env, backend):
     dummy_data = torch.randn(size=(num_layers, 2, num_gpu_blocks, block_size, num_heads, head_size))
 
     worker_datas = [0]
-    for idx in range(1, num_worker):
+    for idx in range(1, num_workers):
         ray.get(workers[idx].execute_method.remote('set_gpu_cache', data=dummy_data))
         worker_datas.append(ray.get(workers[idx].execute_method.remote('get_gpu_cache')))
 
     dst_blocks = list(range(num_gpu_blocks))
     random.shuffle(dst_blocks)
 
-    single_worker_num_blocks = len(dst_blocks)//(num_worker-1)
+    single_worker_num_blocks = len(dst_blocks)//(num_workers-1)
     migration_tasks = []
     worker_idx = 1
     per_step_blocks = 500
@@ -187,3 +187,58 @@ def test_many_to_one_migrate_cache(ray_env, backend):
                 assert torch.allclose(worker_datas[worker_idx][layer_idx][0][src_idx], dst_worker_data[layer_idx][0][dst_idx])
                 assert torch.allclose(worker_datas[worker_idx][layer_idx][1][src_idx], dst_worker_data[layer_idx][1][dst_idx])
         worker_idx += 1
+
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="Need at least 2 GPU to run the test.")
+@pytest.mark.parametrize("backend", ['rayrpc', 'gloo'])
+def test_many_one_to_one_migrate_cache(ray_env, backend):
+    engine_config = EngineArgs(model='facebook/opt-125m', max_model_len=8, enforce_eager=True).create_engine_config()
+    migraiton_config = InstanceArgs(migration_buffer_blocks=3, migration_num_layers=5,
+                                    migration_num_buffers=3).create_migration_config()
+    migraiton_config.migration_backend = backend
+
+    num_workers = 2
+    num_gpu_blocks = 6000
+    workers, _ = get_ready_workers(num_workers, num_gpu_blocks, engine_config, migraiton_config)
+
+    num_layers = engine_config.model_config.get_num_layers(engine_config.parallel_config)
+    head_size = engine_config.model_config.get_head_size()
+    num_heads = engine_config.model_config.get_num_kv_heads(engine_config.parallel_config)
+    block_size = engine_config.cache_config.block_size
+    dummy_data = torch.randn(size=(num_layers, 2, num_gpu_blocks, block_size, num_heads, head_size))
+
+    worker_datas = [0]
+    for idx in range(1, num_workers):
+        ray.get(workers[idx].execute_method.remote('set_gpu_cache', data=dummy_data))
+        worker_datas.append(ray.get(workers[idx].execute_method.remote('get_gpu_cache')))
+
+    dst_blocks = list(range(num_gpu_blocks))
+    random.shuffle(dst_blocks)
+
+    num_migrations = 3
+    single_migration_num_blocks = len(dst_blocks)//(num_migrations-1)
+    migration_tasks = []
+    per_step_blocks = 500
+    for offset in range(0, len(dst_blocks), single_migration_num_blocks):
+        src_to_dst = dict(enumerate(dst_blocks[offset:offset+single_migration_num_blocks]))
+        src_blocks = list(src_to_dst.keys())
+        dst_blocks = list(src_to_dst.values())
+        for idx in range(0, len(src_blocks), per_step_blocks):
+            cur_src_blocks = src_blocks[idx:idx+per_step_blocks]
+            cur_dst_blocks = dst_blocks[idx:idx+per_step_blocks]
+            migration_tasks.append(workers[0].execute_method.remote(
+                'migrate_cache',
+                src_worker_handle_list=[workers[1]],
+                src_blocks=cur_src_blocks,
+                dst_blocks=cur_dst_blocks)
+            )
+    ray.get(migration_tasks)
+
+    dst_worker_data = ray.get(workers[0].execute_method.remote('get_gpu_cache'))
+
+    for offset in range(0, len(dst_blocks), single_migration_num_blocks):
+        src_to_dst = dict(enumerate(dst_blocks[offset:offset+single_migration_num_blocks]))
+
+        for layer_idx in range(num_layers):
+            for src_idx, dst_idx in src_to_dst.items():
+                assert torch.allclose(worker_datas[1][layer_idx][0][src_idx], dst_worker_data[layer_idx][0][dst_idx])
+                assert torch.allclose(worker_datas[1][layer_idx][1][src_idx], dst_worker_data[layer_idx][1][dst_idx])

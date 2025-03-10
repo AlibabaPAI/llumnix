4de53840d9f58ab9addfe795a95c889a40342714
s5u13b <sunbiao.sun@alibaba-inc.com>
2025-03-10 11:38:47 +0000

    Remove test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-export HEAD_NODE_IP='127.0.0.1'
-
-HEAD_NODE=1 python -m llumnix.entrypoints.vllm.api_server \
-                --config-file /mnt/sunbiao.sun/develop/llumnix-github/configs/vllm.yml \
-                --model facebook/opt-125m \
-                --worker-use-ray \

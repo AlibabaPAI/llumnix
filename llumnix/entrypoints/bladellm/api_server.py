734bb809c6504ca7cb7d090aaacc782f26474165
s5u13b <sunbiao.sun@alibaba-inc.com>
2025-03-28 03:30:48 +0000

    Reorg some functions to arg_utils

--- llumnix/entrypoints/bladellm/api_server.py
+++ llumnix/entrypoints/bladellm/api_server.py
@@ -24,12 +24,12 @@ from blade_llm.service.server import Entrypoint
 from llumnix.logging.logger import init_logger
 from llumnix.config import get_llumnix_config
 from llumnix.backends.backend_interface import BackendType
-from llumnix.arg_utils import (EntrypointsArgs, ManagerArgs, LlumnixArgumentParser,
-                               LaunchArgs, InstanceArgs)
+from llumnix.arg_utils import LlumnixArgumentParser, LaunchArgs
 from llumnix.entrypoints.setup import setup_ray_cluster, setup_llumnix
 from llumnix.entrypoints.bladellm.client import LlumnixClientBladeLLM
 from llumnix.entrypoints.utils import EntrypointsContext, LaunchMode, is_gpu_available
-from llumnix.entrypoints.bladellm.arg_utils import BladellmEngineArgs
+from llumnix.entrypoints.bladellm.arg_utils import BladellmEngineArgs, add_cli_args, get_args
+
 
 logger = init_logger(__name__)
 
@@ -90,63 +90,6 @@ async def clean_up_llumnix_components(app):
         # pylint: disable=bare-except
         except:
             pass
-
-
-def add_cli_args(parser: LlumnixArgumentParser) -> LlumnixArgumentParser:
-    parser.set_namespace("llumnix")
-    parser = EntrypointsArgs.add_cli_args(parser)
-    parser = ManagerArgs.add_cli_args(parser)
-    parser = InstanceArgs.add_cli_args(parser)
-    return parser
-
-# Functions detect_unsupported_engine_feature, get_args and setup_llumnix_api_server cannot be put into arg_utils.py,
-# Because they need to import ServingArgs, when importing ServingArgs, it will raise no available gpu error.
-
-def detect_unsupported_engine_feature(engine_args: ServingArgs) -> None:
-    unsupported_feature = None
-    if engine_args.enable_lora:
-        unsupported_feature = "multi-lora serving"
-    elif not engine_args.disable_prompt_cache:
-        unsupported_feature = "automatic prompt caching"
-    elif engine_args.use_sps:
-        unsupported_feature = "speculative decoding"
-    elif engine_args.enable_remote_worker:
-        unsupported_feature = "enable_remote_worker"
-    elif engine_args.enable_hybrid_dp:
-        unsupported_feature = "hybrid data parallel"
-
-    if unsupported_feature:
-        raise ValueError(f'Llumnix does not support "{unsupported_feature}" for BladeLLM currently.')
-
-def get_args(llumnix_cfg, llumnix_parser, engine_args: ServingArgs):
-    instance_args = InstanceArgs.from_llumnix_config(llumnix_cfg)
-    instance_args.init_from_engine_args(engine_args, BackendType.BLADELLM)
-    manager_args = ManagerArgs.from_llumnix_config(llumnix_cfg)
-    manager_args.init_from_instance_args(instance_args)
-    entrypoints_args = EntrypointsArgs.from_llumnix_config(llumnix_cfg)
-
-    EntrypointsArgs.check_args(entrypoints_args, llumnix_parser)
-    instance_args.check_args(instance_args, manager_args, LaunchMode.LOCAL, llumnix_parser)
-    ManagerArgs.check_args(manager_args, llumnix_parser, LaunchMode.LOCAL)
-
-    assert not instance_args.simulator_mode, "Only support the simulator mode for vLLM."
-    assert not (engine_args.enable_disagg and manager_args.enable_pd_disagg), \
-        "Cannot enable both pd-disaggregation inside the LLM engine and pd-disaggregation from Llumnix."
-    assert 'W' not in instance_args.request_migration_policy, \
-        "Migrating waiting request is not supported for BladeLLM temporarily."
-    assert not engine_args.enable_disagg or not manager_args.enable_migration, \
-        "Migration feature is temporarily unavailable for the engine based pd-disaggregation."
-    assert engine_args.pipeline_parallel_size == 1 or not manager_args.enable_migration,\
-        "Migration feature is temporarily unavailable for pipeline parallelism in BladeLLM."
-
-    detect_unsupported_engine_feature(engine_args)
-
-    logger.info("entrypoints_args: {}".format(entrypoints_args))
-    logger.info("manager_args: {}".format(manager_args))
-    logger.info("instance_args: {}".format(instance_args))
-    logger.info("engine_args: {}".format(engine_args))
-
-    return entrypoints_args, manager_args, instance_args, engine_args
 
 def setup_llumnix_api_server(bladellm_args: ServingArgs, loop: asyncio.AbstractEventLoop):
     # generate llumnix_parser for checking parameters with choices

1a4b733c912336fedb98ac4dbf7da92569420edb
tongchenghao <tongchenghao.tch@alibaba-inc.com>
2025-04-14 11:45:08 +0800

    Update llm_engine.py

--- llumnix/backends/bladellm/llm_engine.py
+++ llumnix/backends/bladellm/llm_engine.py
@@ -388,7 +388,9 @@ class BackendBladeLLM(BackendInterface):
     ) -> None:
         self._config_inner_engine_logger(engine_args)
 
-        # add instance_id to avoid path conflict when multi-engine running in a single pod 
+        # add instance_id to avoid path conflict when multi-engine running in a single pod
+        # use instance_id[:5] to avoid the length of worker_socket_path exceeding the OS limit
+        # Note that there is still a small probability that worker_socket_path will be repeated
         engine_args.worker_socket_path = engine_args.worker_socket_path + "_" + str(instance_id)[:5]
         self.instance_id = instance_id
         self.engine_args = engine_args

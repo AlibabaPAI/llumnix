c18c1e152ab82a095351d0bd0b0cb326a331af95
KuilongCui <cuikuilong.ckl@alibaba-inc.com>
2025-04-22 00:02:40 +0800

    fix

--- tools/run_test.sh
+++ tools/run_test.sh
@@ -9,13 +9,19 @@ pgrep -f benchmark_serving.py | { while read pid; do kill -9 "$pid"; done; }
 if [[ "$test_mode" == *"vllm"* ]]; then
     image="registry.cn-beijing.aliyuncs.com/llumnix/llumnix-dev:20250213_image_65d0368"
     install_command="make vllm_install"
+    docker_options="--net host --ipc host"
 elif [[ "$test_mode" == *"bladellm"* ]]; then
     image="registry.cn-beijing.aliyuncs.com/llumnix/llumnix-bladellm-base-dev:202504181632_386530_2b436d6"
     install_command="make bladellm_install"
+    docker_options="--net host --ipc host --device=/dev/infiniband/uverbsX --device=/dev/infiniband/rdma_cm"
 else
     echo "Error: Invalid test_mode '$test_mode'."
     exit 1
 fi
 
-nvidia-docker run --rm -t --net host --ipc host -v ${PWD}:/test_workspace -v /mnt:/mnt -w /test_workspace \
+nvidia-docker run --rm -t \
+  -v ${PWD}:/test_workspace \
+  -v /mnt:/mnt \
+  -w /test_workspace \
+  $docker_options \
   "$image" sh -c "$install_command > /dev/null && make $test_mode"

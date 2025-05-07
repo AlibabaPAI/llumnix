58bb487c7e8b0d9d4aab5d7e44a128cc8d86daf8
tongchenghao <tongchenghao.tch@alibaba-inc.com>
2025-05-07 12:35:54 +0000

    use task to remote launch bladellm

--- llumnix/entrypoints/bladellm/serve.py
+++ llumnix/entrypoints/bladellm/serve.py
@@ -1,19 +1,27 @@
 import time
+import sys
+import os
 import pickle
+import ray
 from ray.util.queue import Queue as RayQueue
-
-from blade_llm.service.args import ServingArgs, add_args
-from blade_llm.service.server import check_ports
 
 from llumnix.entrypoints.bladellm.arg_utils import add_llumnix_cli_args, get_args, BladellmEngineArgs
 from llumnix.arg_utils import LlumnixArgumentParser, LaunchArgs
+from llumnix.logging.logger import init_logger
 from llumnix.config import get_llumnix_config
 from llumnix.entrypoints.utils import LaunchMode
 from llumnix.backends.backend_interface import BackendType
 from llumnix.entrypoints.setup import connect_to_ray_cluster, setup_llumnix
 
+logger = init_logger('llumnix.entrypoints.bladellm.server')
 
-if __name__ == "__main__":
+# Assume that there is an existing ray cluster when using centralized deployment.
+connect_to_ray_cluster()
+
+def main():
+    # pylint: disable=import-outside-toplevel
+    from blade_llm.service.args import ServingArgs, add_args
+    from blade_llm.service.server import check_ports
     parser = add_args()
     cli_args = parser.parse_args()
     engine_args = ServingArgs.from_cli_args(cli_args)
@@ -26,9 +34,6 @@ if __name__ == "__main__":
     parser = LlumnixArgumentParser()
     parser = add_llumnix_cli_args(parser)
     llumnix_config = get_llumnix_config(engine_args.llumnix_config, cli_args=engine_args.llumnix_opts)
-
-    # Assume that there is an existing ray cluster when using centralized deployment.
-    connect_to_ray_cluster()
 
     entrypoints_args, manager_args, instance_args, engine_args = get_args(llumnix_config, LaunchMode.GLOBAL, parser, engine_args)
     launch_args = LaunchArgs(launch_mode=LaunchMode.GLOBAL, backend_type=BackendType.BLADELLM)
@@ -48,3 +53,44 @@ if __name__ == "__main__":
     if not entrypoints_args.disable_keep_serve_process_alive:
         while True:
             time.sleep(100.0)
+
+
+@ray.remote(num_cpus=1)
+def remote_launch_task(serve_args):
+    # set sys.argv
+    sys.argv = ['llumnix.entrypoints.bladellm.serve'] + serve_args
+
+    # avoid "RuntimeError: No CUDA GPUs are available"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    main()
+
+def get_current_node_resources():
+    current_node_id = ray.get_runtime_context().get_node_id()
+    resource = {}
+    for node in ray.nodes():
+        if node["NodeID"] == current_node_id:
+            resource = node["Resources"]
+            break
+    return resource.get("GPU", 0), resource.get("CPU", 0)
+
+
+if __name__ == "__main__":
+
+    gpu_num, cpu_num = get_current_node_resources()
+
+    if gpu_num > 0:
+        # current node has GPU resources
+        logger.info("launch on current node.")
+        main()
+    elif cpu_num == 0 and gpu_num == 0:
+        # currnet node has no CPU resources and no GPU resources, it is highly likely that the current node is the head node
+        # bladellm can only run on GPU nodes, so use a ray task to launch bladellm on other nodes
+        logger.info("no gpu on current node, launch on another node.")
+        # get args
+        original_argv = sys.argv[1:]
+        ray.get(remote_launch_task.remote(original_argv))
+    else:
+        logger.info(
+            "currnet node has CPU resources but no GPU resources, not support now."
+        )

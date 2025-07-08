485788223935783e5ab062f5455ec44fce41ef3e
Zhao Zhiyu <zhaozhiyu.zy@alibaba-inc.com>
2025-07-08 14:40:15 +0800

    add backends/vllm_utils.py; move stuffs from backends/vllm_v1/utils.py to backends/vllm_utils.py; delete backends/vllm_v1/utils.py

--- llumnix/backends/vllm_utils.py
+++ llumnix/backends/vllm_utils.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from functools import wraps
 from typing import Dict, List, Optional
 import torch
 

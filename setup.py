2c38bd6a1f6229229c63a85485bbd859b5e8e21f
KuilongCui <cuikuilong.ckl@alibaba-inc.com>
2025-07-10 09:21:50 +0000

    version change

--- setup.py
+++ setup.py
@@ -15,11 +15,24 @@ import os
 import subprocess
 from typing import List
 
+import setuptools_scm
 from setuptools import setup, find_packages
 from wheel.bdist_wheel import bdist_wheel
 
 ROOT_DIR = os.path.dirname(__file__)
+VERSION_SCHEME = setuptools_scm.version.guess_next_dev_version
+LOCAL_SCHEME = setuptools_scm.version.get_local_node_and_date
 
+
+# To auto generate version like 1.0.1.dev2+g1234abc {guessed_next_ver}.dev{distance}+g{git_hash}
+# and write into blade_llm/version.py
+def scm_version():
+    return {
+        'version_scheme': VERSION_SCHEME,
+        'local_scheme': LOCAL_SCHEME,
+        'write_to': 'llumnix/version.py',
+        'write_to_template': '__version__ = {version!r}',
+    }
 
 def get_path(*filepath) -> str:
     return os.path.join(ROOT_DIR, 'requirements', *filepath)
@@ -42,7 +55,7 @@ class BuildWheelOverride(bdist_wheel):
 
 setup(
     name='llumnix',
-    version='0.0.2',
+    use_scm_version=scm_version,
     python_requires='>=3.9.0, <=3.12.3',
     description='Efficient and easy multi-instance LLM serving',
     long_description=readme(),

1229239213e5185b061c8c8652e3bc18062f83d1
KuilongCui <cuikuilong.ckl@alibaba-inc.com>
2025-07-28 03:23:55 +0000

    fix lint

--- llumnix/llumlet/local_migration_scheduler.py
+++ llumnix/llumlet/local_migration_scheduler.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Deque, List
+from typing import Deque, List, Optional
 import numpy as np
 
 from llumnix.llumlet.request import LlumnixRequest, RequestStatus, RequestInferenceType

8be3e6205417dddacb56ac357f5a26fec177803b
城皓 <tongchenghao.tch@alibaba-inc.com>
2025-08-07 22:39:36 +0800

    clean code

--- llumnix/backends/output_forwarder.py
+++ llumnix/backends/output_forwarder.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, Iterable, List, Coroutine, Union
+from typing import Dict, List, Coroutine, Union
 import asyncio
 import queue
 import threading

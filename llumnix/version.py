98abc9dfbe67314fc2f0394370f5d09e9fb343aa
KuilongCui <cuikuilong.ckl@alibaba-inc.com>
2025-08-11 06:54:57 +0000

    fix version

--- llumnix/version.py
+++ llumnix/version.py
@@ -11,4 +11,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.1.dev173+gf59f686.d20250811'
+__version__ = '0.1.1.dev165+g2586810.d20250801'
